63460c2396749f0a0d046d43eb507af885546992
Setiawan, Fendy <fsetiawan@gatech.edu>
2022-08-15 21:39:32 -0700

    count expenses by category 3

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -7,12 +7,11 @@ from . import Expense
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
 
-spending_categories = list()
+spending_categories = []
 for expense in expenses.list:
     spending_categories.append(expense.category)
 
 spending_counter = collections.Counter(spending_categories)
-
 print(spending_counter)
 
 top5 = spending_counter.most_common(5)
